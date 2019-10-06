61299118c431c87f2844e329670c93c26c2abcff
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-10-06 16:52:26 +0100

    Remove debug logs

--- src/data_access/db_client.ts
+++ src/data_access/db_client.ts
@@ -8,10 +8,8 @@ const connectToDbClient = async () => {
   try {
     await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     logger.info(SuccessMessage.DB_CONNECTION);
-    logger.debug(MONGO_URI)
   } catch (err) {
     logger.error(`${ErrorMessage.DB_CONNECTION}: ${err}`)
-    logger.debug(MONGO_URI)
   }
 };
 
