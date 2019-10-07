2c7611ac9699af1288211a7d84d309ff13131b0e
rayanabdirahman <rayanmabdirahman@gmail.com>
2019-10-07 12:07:00 +0100

    Add comments to user service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,6 +2,10 @@ import User from './../data_access/model/user.model';
 import { ISignUpModel } from '../domain/interfaces';
 
 export default class UserService {
+  /**
+   * Register user to database
+   * @param {ISignUpModel} model - user signup information 
+   */
   public async signUp(model: ISignUpModel) {
     const user = new User(model);
 
